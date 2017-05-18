8ddda8abaf879632f01a1c5b841f1b77f6d984a3
levesqu6 <blevesqu@gmail.com>
2017-05-17 21:47:19 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -15,14 +15,14 @@ from Queue import Queue
 #================ Global Variables ==================
 lognum = 1
 avitime = 0
-logdir = '/tmp/'
+logdir = '/mnt/logs/'
 logbasename = 'log'
 logext = '.vbo'
 
-start_speed_threshold = 45 #km/hr
+start_speed_threshold = 0 #km/hr
 start_delay = 2 #seconds
 
-stop_speed_threshold = 40 #km/hr
+stop_speed_threshold = 0 #km/hr
 stop_delay = 3 #seconds
 
 queue = Queue(10)
@@ -75,27 +75,27 @@ class GpsConsumer(Thread):
 			curtime =time()
 			curspeed = (gpsd.fix.speed * 3.6)
 
-			print curtime, (curtime - timerstart), (curtime - timerstop), (gpsd.fix.speed * 3.6)
+			#print curtime, (curtime - timerstart), (curtime - timerstop), (gpsd.fix.speed * 3.6)
 
 			if (not logging) and  (curspeed > start_speed_threshold) and (timerstart==0): #start timer
 				timerstart = curtime
-				print "Start Timer Marked"
+				#print "Start Timer Marked"
 			elif (not logging) and  (curspeed <= start_speed_threshold) and (timerstart > 0): #restart time if time dips
 				timerstart = 0
-				print "Start Timer Restarted"
+				#print "Start Timer Restarted"
 			elif (not logging) and  (curspeed > start_speed_threshold) and (timerstart!=0) and ((curtime - timerstart) > start_delay): #start a log if timer lapsed
 				log = LogWriter(logdir, logbasename) # create log
 				logging = True
 				timerstart = 0
 			elif logging and (curspeed < stop_speed_threshold) and (timerstop == 0): #if speed drops start timing
 				timerstop = curtime
-				print "Stop Timer Marked"
+				#print "Stop Timer Marked"
 			elif logging and (curspeed >= stop_speed_threshold) and (timerstop > 0): #if speed recovers stop timing
 				timerstop = 0
-				print "Stop Timer Restarted"
+				#print "Stop Timer Restarted"
 			elif logging and (curspeed < stop_speed_threshold) and (timerstop!=0) and ((curtime - timerstop) > stop_delay): #stop log after timer lapses
 				timerstop = 0
-				print "Ending Log"
+				#print "Ending Log"
 				logging = False
 				log.end_log()
 
@@ -117,7 +117,7 @@ class LogWriter():
 		while filename in files:
 			filename = filename.replace(filename[len(logbasename):-len(logext)],str(int(filename[len(logbasename):-len(logext)]) + 1).zfill(4))
 			lognum = lognum + 1
-		print("Starting Log File: " + logdir + filename)
+		#print("Starting Log File: " + logdir + filename)
 
 	        #open log file
 		self.csv_file = open(logdir + filename, 'wt',1) #1 = write line at a time
@@ -166,12 +166,12 @@ sats time lat long velocity heading height avifileindex avitime\n\
 	def write_row(self,gpsd):
 		global avitime
 		data_string = self.build_string(gpsd)
-		print(data_string)
+		#print(data_string)
                 self.csv_file.write(data_string + '\n')
                 avitime = avitime + 1 * 1000
 	def end_log(self):
 		self.csv_file.close()
-		print("CSV Log File Closed")
+		#print("CSV Log File Closed")
 
 	def __del__(self):
 		self.csv_file.close()
